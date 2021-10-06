2ac32ea0c98b7b741a5af2cb4408806e5fc5e3a1
timotius <timotis@ruangalternative.com>
2021-10-06 12:32:57 +0700

    add chat list page

---
+++ src/components/ChatBox/index.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import {StyleSheet, Text, View, Image, TouchableOpacity} from 'react-native';
+import dummyPP from '../../assets/images/dummyPP.jpg';
+import {MAIN_COLOR} from '../../assets/constant';
+
+const ChatBox = ({name, newMsg, num, time}) => {
+  return (
+    <TouchableOpacity style={styles.container}>
+      <View style={{flexDirection: 'row'}}>
+        <Image source={dummyPP} style={styles.pp} />
+        <View style={{padding: 10}}>
+          <Text style={styles.name}>{name}</Text>
+          <Text style={styles.msg}>{newMsg}</Text>
+        </View>
+      </View>
+      <View style={{paddingTop: 5}}>
+        <Text>{time}</Text>
+        <View style={{flexDirection: 'row', justifyContent: 'center'}}>
+          <View style={styles.unread}>
+            <Text>{num}</Text>
+          </View>
+        </View>
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+export default ChatBox;
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginBottom: 10,
+  },
+  pp: {
+    width: 70,
+    height: 70,
+    borderRadius: 15,
+  },
+  name: {
+    fontFamily: 'Rubik-SemiBold',
+    color: 'black',
+    fontSize: 18,
+    paddingLeft: 10,
+  },
+  msg: {
+    color: MAIN_COLOR,
+    paddingLeft: 10,
+  },
+  unread: {
+    backgroundColor: MAIN_COLOR,
+    paddingHorizontal: 10,
+    paddingVertical: 3,
+    borderRadius: 13,
+    marginTop: 2,
+  },
+});
--- src/pages/ChatList/index.js
+++ src/pages/ChatList/index.js
@@ -1,16 +1,49 @@
-import React from 'react';
-import {StyleSheet, Text, View, TouchableOpacity} from 'react-native';
+import React, {useState} from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  TouchableOpacity,
+  TextInput,
+} from 'react-native';
+import {MAIN_COLOR, SCREEN_HEIGHT, SCREEN_WIDTH} from '../../assets/constant';
+import ChatBox from '../../components/ChatBox';
+import IconMenu from '../../assets/icons/Menu.svg';
+import IconPlus from '../../assets/icons/Plus.svg';
+import IconSearch from '../../assets/icons/Search.svg';
 
 const ChatList = ({navigation}) => {
   return (
-    <View>
-      <TouchableOpacity
-        style={styles.tmbl}
-        onPress={() => {
-          navigation.navigate('ChatRoom');
-        }}>
-        <Text>ChatList</Text>
-      </TouchableOpacity>
+    <View style={styles.container}>
+      <View style={styles.head}>
+        <Text style={styles.title}>SimpleTalk</Text>
+        <TouchableOpacity>
+          <IconMenu height={28} width={28} />
+        </TouchableOpacity>
+      </View>
+      <View style={styles.searchContainer}>
+        <TouchableOpacity style={styles.iconPlus}>
+          <IconSearch height={20} width={20} />
+        </TouchableOpacity>
+        <TextInput style={styles.input} placeholder="Type your message..." />
+        <TouchableOpacity style={styles.iconPlus}>
+          <IconPlus height={20} width={20} />
+        </TouchableOpacity>
+      </View>
+      <ScrollView style={{marginBottom: 40}}>
+        <ChatBox name="Kevin" newMsg="Hei..." num={20} time="20:10" />
+        <ChatBox name="Kevin" newMsg="Hei..." num={20} time="20:10" />
+        <ChatBox name="Kevin" newMsg="Hei..." num={20} time="20:10" />
+        <ChatBox name="Kevin5" newMsg="Hei..." num={20} time="20:10" />
+        <ChatBox name="Kevin4" newMsg="Hei..." num={20} time="20:10" />
+        <ChatBox name="Kevin3" newMsg="Hei..." num={20} time="20:10" />
+        <ChatBox name="Kevin2" newMsg="Hei..." num={20} time="20:10" />
+        <ChatBox name="Kevin4" newMsg="Hei..." num={20} time="20:10" />
+        <ChatBox name="Kevin3" newMsg="Hei..." num={20} time="20:10" />
+        <ChatBox name="Kevin2" newMsg="Hei..." num={20} time="20:10" />
+        <ChatBox name="No One" newMsg="Hei come..." num={20} time="20:10" />
+      </ScrollView>
     </View>
   );
 };
@@ -18,7 +51,37 @@ const ChatList = ({navigation}) => {
 export default ChatList;
 
 const styles = StyleSheet.create({
-  tmbl: {
-    backgroundColor: 'red',
+  container: {
+    marginTop: 15,
+    marginHorizontal: 15,
+    height: SCREEN_HEIGHT,
+  },
+  head: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginTop: 35,
+    marginBottom: 25,
+  },
+  title: {
+    fontFamily: 'Rubik-SemiBold',
+    fontSize: 30,
+    color: MAIN_COLOR,
+  },
+  searchContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginBottom: 20,
+  },
+  input: {
+    width: '80%',
+    height: 40,
+    borderRadius: 8,
+    fontSize: 16,
+  },
+  iconPlus: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    paddingRight: 5,
   },
 });
